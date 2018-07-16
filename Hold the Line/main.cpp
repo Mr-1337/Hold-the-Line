7d7759d716d5c1305a0a4cb6f35cba731b0dc9bd
Mr-1337 <kyanisawesome@gmail.com>
2018-07-16 17:48:35 -0400

    Installed SDL and SDL extension libraries

---
+++ Hold the Line/main.cpp
@@ -0,0 +1,13 @@
+//Application Entry Point
+//7/16/2018
+//Hold the Line is a smallish scale strategy game about maximizing resource intake and defending your territory
+
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cin.get();
+	return 0;
+}
